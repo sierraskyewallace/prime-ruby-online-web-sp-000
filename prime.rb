c6480aa7d3cf494b207df12eda409de102f970ce
Sierra Wallace <sskwallace@gmail.com>
2020-08-20 22:46:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2 
+    return false
+    elsif (2..integer-1).any?{|num| integer % num == 0}
+    return false
+  else
+    return true
+  end
+end
+
+
+  
